7df8481572e844c31241a56cd2288d6b17723df8
aidan <grimshawaidan@gmail.com>
2018-11-25 21:39:39 -0800

    almost done with implementation

--- drivers/leds/trigger/ledtrig-morse.c
+++ drivers/leds/trigger/ledtrig-morse.c
@@ -22,6 +22,16 @@
 #include <linux/reboot.h>
 #include "../leds.h"
 
+#include <linux/cdev.h>
+#include <linux/uaccess.h>
+
+#define FIRST_MINOR 0
+#define MINOR_CNT 1
+
+static dev_t dev;
+static struct cdev c_dev;
+static struct class *cl;
+
 static int panic_morses;
 
 struct morse_trig_data {
@@ -29,6 +39,8 @@ struct morse_trig_data {
 	unsigned int period;
 	struct timer_list timer;
 	unsigned int invert;
+	unsigned int speed;
+	unsigned int mode;
 };
 
 static const int message[18] = {
@@ -64,13 +76,17 @@ static void led_morse_function(unsigned long data)
 		onOff = 0;
 	}
 	brightness = onOff;
-	// wrap around message
+	// wrap around message if in repeat mode
 	if (myIndex == 18){
 		myIndex = 0;
 	}
+	// if not in repeat mode and at end of message, turn off
+	if (morse_data->mode == 1){
+		brightness = LED_OFF;
+	}
 	delay = msecs_to_jiffies(message[myIndex]);
 	// if speed is selected, multiply delay by two to make morse print slower
-	if (morse_data->invert == 1){
+	if (morse_data->speed == 1){
 		// brightness = LED_OFF;
 		delay = msecs_to_jiffies(message[myIndex]*2);
 	}
@@ -82,6 +98,56 @@ static void led_morse_function(unsigned long data)
 	mod_timer(&morse_data->timer, jiffies + delay);
 }
 
+static ssize_t led_speed_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	struct led_classdev *led_cdev = dev_get_drvdata(dev);
+	struct morse_trig_data *morse_data = led_cdev->trigger_data;
+
+	return sprintf(buf, "%u\n", morse_data->speed);
+}
+
+static ssize_t led_speed_store(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t size)
+{
+	struct led_classdev *led_cdev = dev_get_drvdata(dev);
+	struct morse_trig_data *morse_data = led_cdev->trigger_data;
+	unsigned long state;
+	int ret;
+
+	ret = kstrtoul(buf, 0, &state);
+	if (ret)
+		return ret;
+
+	morse_data->speed = !!state;
+
+	return size;
+}
+static ssize_t led_mode_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	struct led_classdev *led_cdev = dev_get_drvdata(dev);
+	struct morse_trig_data *morse_data = led_cdev->trigger_data;
+
+	return sprintf(buf, "%u\n", morse_data->mode);
+}
+
+static ssize_t led_mode_store(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t size)
+{
+	struct led_classdev *led_cdev = dev_get_drvdata(dev);
+	struct morse_trig_data *morse_data = led_cdev->trigger_data;
+	unsigned long state;
+	int ret;
+
+	ret = kstrtoul(buf, 0, &state);
+	if (ret)
+		return ret;
+
+	morse_data->mode = !!state;
+
+	return size;
+}
 static ssize_t led_invert_show(struct device *dev,
 		struct device_attribute *attr, char *buf)
 {
@@ -108,10 +174,66 @@ static ssize_t led_invert_store(struct device *dev,
 	return size;
 }
 
+static int my_open(struct inode *i, struct file *f)
+{
+    printk("CS444 Dummy driver open\r\n");
+    return 0;
+}
+static int my_close(struct inode *i, struct file *f)
+{
+    printk("CS444 Dummy driver close\r\n");
+    return 0;
+}
+
+static ssize_t dummy_read(struct file *file, char __user *buf, size_t size, loff_t *ppos)
+{
+    printk("CS444 Dummy driver read\r\n");
+    snprintf(buf, size, "Hey there, I'm a dummy!\r\n");
+    return strlen(buf);
+}
+
+static ssize_t dummy_write(struct file *file, const char __user *buf, size_t size, loff_t *ppos)
+{
+    char lcl_buf[64];
+
+    memset(lcl_buf, 0, sizeof(lcl_buf));
+
+    if (copy_from_user(lcl_buf, buf, min(size, sizeof(lcl_buf))))
+        {
+            return -EACCES;
+        }
+
+    printk("CS444 Dummy driver write %ld bytes: %s\r\n", size, lcl_buf);
+
+    return size;
+}
+
+static struct file_operations dummy_fops =
+{
+    .owner = THIS_MODULE,
+    .open = my_open,
+    .read = dummy_read,
+    .write = dummy_write,
+    .release = my_close
+};
+
 static DEVICE_ATTR(invert, 0644, led_invert_show, led_invert_store);
-static DEVICE_ATTR(speed, 0644, led_invert_show, led_invert_store);
+static DEVICE_ATTR(speed, 0644, led_speed_show, led_speed_store);
+static DEVICE_ATTR(mode, 0644, led_mode_show, led_mode_store);
+
 static void morse_trig_activate(struct led_classdev *led_cdev)
 {
+	// creating character device start		
+	alloc_chrdev_region(&dev, 0, 1, "cs444_dummy");
+
+	cdev_init(&c_dev, &dummy_fops);
+	cdev_add(&c_dev, dev, MINOR_CNT);
+
+	cl = class_create(THIS_MODULE,"char");
+	device_create(cl,NULL,dev,NULL,"morse");
+	printk("CS444 Dummy Driver has been loaded!\r\n");
+	// create character device end
+
 	struct morse_trig_data *morse_data;
 	int rc;
 
@@ -122,6 +244,7 @@ static void morse_trig_activate(struct led_classdev *led_cdev)
 	led_cdev->trigger_data = morse_data;
 	rc = device_create_file(led_cdev->dev, &dev_attr_invert);
 	device_create_file(led_cdev->dev, &dev_attr_speed);
+	device_create_file(led_cdev->dev, &dev_attr_mode);
 	if (rc) {
 		kfree(led_cdev->trigger_data);
 		return;
@@ -139,6 +262,13 @@ static void morse_trig_activate(struct led_classdev *led_cdev)
 
 static void morse_trig_deactivate(struct led_classdev *led_cdev)
 {
+	// destroy character device start
+	device_destroy(cl, dev);
+    class_destroy(cl);
+    cdev_del(&c_dev);
+    unregister_chrdev_region(dev, MINOR_CNT);
+	// destroy character device end
+
 	struct morse_trig_data *morse_data = led_cdev->trigger_data;
 
 	if (led_cdev->activated) {
@@ -180,11 +310,6 @@ static struct notifier_block morse_panic_nb = {
 
 static int __init morse_trig_init(void)
 {
-	alloc_chrdev_region(&dev, 0, 1, "cs444_dummy");
-
-	struct class *cl = class_create(THIS_MODULE,"dummy");
-	device_create(cl,NULL,dev,NULL,"d");
-
 	int rc = led_trigger_register(&morse_led_trigger);
 
 	if (!rc) {
